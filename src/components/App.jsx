7a03b06294b39df5b8f833f29d471cdf0c534ed7
IhorKozloff <ik.eyeshield21@gmail.com>
2022-04-12 23:28:41 +0300

    осталось разъединить

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,78 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from "react";
+import { ContactsForm } from "components/Form/Form";
+
+export class App extends Component  {
+  state = {
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    filter:'',
+  }
+  
+
+onAddNewContact= (dataForNewContact) => {
+  const namesList = this.state.contacts.map(item => item.name.toLocaleLowerCase());
+  const addedName = dataForNewContact.name.toLocaleLowerCase();
+
+  if (namesList.includes(addedName)) {
+    return alert(`${dataForNewContact.name} is already in contacts`)
+  } else {
+    this.setState(oldState => {
+      return {contacts: [...oldState.contacts, dataForNewContact]}
+    });
+  }
+};
+
+  onFilterChange = (event) => {
+    this.setState({filter: event.target.value})
+  }
+  onDeleteContact = (deletedItemId) => {
+    this.setState(oldState => {
+      return {contacts: [...oldState.contacts].filter(item => {
+        return item.id !== deletedItemId
+      })}
+    });
+  }
+
+  contactsRender = () => {
+    return this.state.contacts.map(item => {
+      if(item.name.toLowerCase().includes(this.state.filter)) {
+        return (
+          <li key={item.id} id={item.id}>
+              {item.name}: {item.number}
+              <button type="button" onClick={() => {
+                this.onDeleteContact(item.id)
+              }}>Delete</button>
+          </li>
+        )
+      } else {
+        return ''
+      }
+      
+    })
+  }
+
+  render () {
+  
+    return (
+      <>
+        
+        <ContactsForm onAddNewContact={this.onAddNewContact}/>
+        <label>
+          Find contacts by Name
+          <input onChange={this.onFilterChange}></input>
+        </label>
+        <h1>Contacts</h1>
+        <ul>
+          {this.contactsRender()}
+        </ul>
+      </>
+      
+    )
+
+
+  };
 };
---
+++ src/components/Form/Form.styled.jsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled'
+import { Field } from 'formik'
+
+
+export const InputStyled = styled(Field)`
+    color: red;
+`;
